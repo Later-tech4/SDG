20a79a982366f1e32b0440c92c299ac220c122d5
Later-tech4 <92619366+Later-tech4@users.noreply.github.com>
2021-10-16 14:52:49 +0400

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,41 @@
+a=(input("Good Morning Sir/Madam "))
+print("# put only first letter in CAPS , eg-Goal 1")
+SDG="SDG - The 17 sustainable development goals (SDGs) to transform our world"
+SDG_1="GOAL 1: No Poverty, GOAL 2: Zero Hunger , GOAL 3: Good Health and Well-being, GOAL 4: Quality Education, GOAL 5: Gender Equality, GOAL 6: Clean Water and Sanitation, GOAL 7: Affordable and Clean Energy, GOAL 8: Decent Work and Economic Growth, GOAL 9: Industry, Innovation and Infrastructure, GOAL 10: Reduced Inequality, GOAL 11: Sustainable Cities and Communities, GOAL 12: Responsible Consumption and Production, GOAL 13: Climate Action, GOAL 14: Life Below Water, GOAL 15: Life on Land, GOAL 16: Peace and Justice Strong Institutions, GOAL 17: Partnerships to achieve the Goal"
+print(SDG)
+print(SDG_1)
+b=(input("Which SDG Goal's information , do you want to know "))
+if b=="Goal 1":
+  print("Economic growth must be inclusive to provide sustainable jobs and promote equality.")
+elif b=="Goal 2":
+  print("The food and agriculture sector offers key solutions for development, and is central for hunger and poverty eradication.")
+elif b=="Goal 3":
+  print("Ensuring healthy lives and promoting the well-being for all at all ages is essential to sustainable development.")
+elif b=="Goal 4":
+  print("Obtaining a quality education is the foundation to improving people’s lives and sustainable development.")
+elif b=="Goal 5":
+  print("Gender equality is not only a fundamental human right, but a necessary foundation for a peaceful, prosperous and sustainable world.")
+elif b=="Goal 6":
+  print("Clean, accessible water for all is an essential part of the world we want to live in.")
+elif b=="Goal 7":
+  print("Energy is central to nearly every major challenge and opportunity.")
+elif b=="Goal 8":
+  print("Sustainable economic growth will require societies to create the conditions that allow people to have quality jobs.")
+elif b=="Goal 9":
+  print("Investments in infrastructure are crucial to achieving sustainable development.")
+elif b=="Goal 10":
+  print("To reduce inequalities, policies should be universal in principle, paying attention to the needs of disadvantaged and marginalized populations.")
+elif b=="Goal 11":
+  print("There needs to be a future in which cities provide opportunities for all, with access to basic services, energy, housing, transportation and more.")
+elif b=="Goal 12":
+  print("Responsible Production and Consumption")
+elif b=="Goal 13":
+  print("Climate change is a global challenge that affects everyone, everywhere.")
+elif b=="Goal 14":
+  print("Careful management of this essential global resource is a key feature of a sustainable future.")
+elif b=="Goal 15":
+  print("Sustainably manage forests, combat desertification, halt and reverse land degradation, halt biodiversity loss")
+elif b=="Goal 16":
+  print("Access to justice for all, and building effective, accountable institutions at all levels.")
+elif b=="Goal 17":
+  print("Revitalize the global partnership for sustainable development.")
